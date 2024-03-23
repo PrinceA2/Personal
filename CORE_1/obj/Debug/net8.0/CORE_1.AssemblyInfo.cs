358a206808be78cbf97d62da49dcc6f28cbccbee
Prince <tiwariprince938@gmail.com>
2024-03-23 21:10:12 +0530

    Everything commited

--- CORE_1/obj/Debug/net8.0/CORE_1.AssemblyInfo.cs
+++ CORE_1/obj/Debug/net8.0/CORE_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CORE_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2cf3216826b629aeeaf20eaed373220b8ffbd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CORE_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CORE_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
